220f198d979967e04de1cec53a845b040b99059c
Didier Defrance <didier@d-defrance.fr>
2022-04-15 14:00:37 +0200

    equation

--- tp-js/jsWeb/js/equation_jq.js
+++ tp-js/jsWeb/js/equation_jq.js
@@ -1,23 +1,76 @@
 $(function(){
    //fonction anonyme déclenchée automatiquement au chargement de la page
-   //....
-
+   $("#btnRefresh").on("click",reactualiserTableau);
 })
 
 function reactualiserTableau(){
-    let minX ;
-    let maxX; 
-    //...
-    //...
+    let minX = Number($("#inputMinX").val());
+    let maxX = Number($("#inputMaxX").val());
+    let minY=0;
+    let maxY=0;
+    let pointValues=[]; 
     let n= 10;
     let dx  = (maxX - minX) / n ;
     // enlever les éventuelles anciennes lignes du tableau:
-    //...
+    $("tbody").html("");
+
     for(let x= minX; x <= maxX; x = x + dx ){
         let y = calculerYselonX(x);
-        //ajouter une ligne dans le tableau
+        $("tbody").append("<tr><td>"+x+"</td><td>"+y+"</td></tr>");
+        pointValues.push({x:x,y:y});
+        if(y<=minY) minY=y;
+		if(y>=maxY) maxY=y;
     }
-    //...
+    var dy=(maxY-minY)/100;
+    reactualiserCourbe(pointValues,minX,maxX,minY,maxY,dx,dy);
+}
+
+function reactualiserCourbe(pointValues,minX,maxX,minY,maxY,dx,dy){
+    var ctx = document.getElementById("myChart").getContext('2d');
+    try{
+        Chart.instances[0].destroy();
+     }
+     catch(e){}
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          datasets: [{
+            label: 'y=f(x)='+$("#inputY").val(),
+            data: pointValues,
+            borderColor: [
+              'rgba(33, 232, 234, 1)',
+              'rgba(33, 232, 234, 1)',
+              'rgba(33, 232, 234, 1)',
+              'rgba(33, 232, 234, 1)',
+              'rgba(33, 232, 234, 1)',
+              'rgba(33, 232, 234, 1)'
+            ],
+            borderWidth: 1
+          }],
+        },
+        options: {
+          scales: {
+            x: {
+              type: 'linear',
+              position: 'bottom',
+              min: minX,
+              max: maxX,
+              ticks: {
+                stepSize: dx*10,
+                fixedStepSize: dx*10,
+              }
+            },
+            yAxes: {
+              min: minY,
+              max: maxY,
+              ticks: {
+                stepSize: dy*10,
+                fixedStepSize: dy*10,
+              }
+            }
+          }
+        }
+      });
 }
 
 function calculerYselonX(x){
