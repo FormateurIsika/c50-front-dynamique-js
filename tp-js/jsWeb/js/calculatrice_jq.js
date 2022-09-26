95283cad727258982507bd404e89d32386c90e73
DidierDefrance <didier@d-defrance.fr>
2022-09-26 14:37:51 +0200

    tp tableau (debut)

--- tp-js/jsWeb/js/calculatrice_jq.js
+++ tp-js/jsWeb/js/calculatrice_jq.js
@@ -27,9 +27,9 @@ $(function() {
    $('#btnDiv').on("click",function(){ calculer('/');  });
 
    $('#cbHisto').on("change",function(evt){ 
-       //var estcoche = evt.target.checked;
-       var estCoche = $("#cbHisto").prop("checked");
-       //var estCoche = ($("#cbHisto").get(0)).checked;
+       var estCoche = evt.target.checked; // api DOM
+       //var estCoche = $("#cbHisto").prop("checked"); //api JQ
+       //var estCoche = ($("#cbHisto").get(0)).checked; // api DOM apres api js vers api DOM
        console.log("estCoche="+estCoche);
         //V1:
         //$("#ulHistorique").css("display",estCoche?"block":"none");
@@ -41,11 +41,13 @@ $(function() {
            $("#ulHistorique").hide();
        */
         //V3 (avec effet ):
+        
         if(estCoche)
             //$("#ulHistorique").fadeIn(1800);//400ms par defaut
             $("#ulHistorique").slideDown(800);//400ms par defaut
          else
            //$("#ulHistorique").fadeOut(1800);
            $("#ulHistorique").slideUp(800);//400ms par defaut
+        
    });
 });
---
+++ tp-js/jsWeb/js/tableau_jq.js
@@ -0,0 +1,16 @@
+var tabProduits = [
+    { ref : 'p1' , nom : 'stylo' , prix : 2.3},
+    { ref : 'p2' , nom : 'cahier' , prix : 3.3},
+    { ref : 'p3' , nom : 'classeur' , prix : 5.3}
+]
+
+$( function(){
+   //code déclenché automatiquement au chargement de la page dans le navigateur
+
+   //console.log(tabProduits.length);
+   //Tp à faire :
+   //ecrire un boucle qui ajoute chaque partie du tabProduits
+   //en tant que nouvelle ligne du tableau html.
+   //suggestion: on peut appeler .append("<tr>+ ... + </tr>") sur #tBodyProd
+}
+)
