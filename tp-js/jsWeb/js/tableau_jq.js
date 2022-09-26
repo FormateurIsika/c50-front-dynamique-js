4ebdfa7c874a1b9ae6548f4d0546b9c8e60e5b3c
DidierDefrance <didier@d-defrance.fr>
2022-09-26 14:50:03 +0200

    s

--- tp-js/jsWeb/js/tableau_jq.js
+++ tp-js/jsWeb/js/tableau_jq.js
@@ -12,5 +12,12 @@ $( function(){
    //ecrire un boucle qui ajoute chaque partie du tabProduits
    //en tant que nouvelle ligne du tableau html.
    //suggestion: on peut appeler .append("<tr>+ ... + </tr>") sur #tBodyProd
+   //for(let i =0; i<tabProduits.length; i++){
+   for(let i in tabProduits){
+        let p = tabProduits[i];
+        $("#tBodyProd").append("<tr><td>"+p.ref+"</td><td>"+p.nom+
+                                    "</td><td>"+p.prix+"</td></tr>");
+   }
+
 }
 )
