d93218f36549b3b8347027a47b26aada91923652
DidierDefrance <didier@d-defrance.fr>
2022-09-26 15:34:28 +0200

    s

--- tp-js/jsWeb/js/tableau_jq.js
+++ tp-js/jsWeb/js/tableau_jq.js
@@ -15,8 +15,23 @@ $( function(){
    //for(let i =0; i<tabProduits.length; i++){
    for(let i in tabProduits){
         let p = tabProduits[i];
-        $("#tBodyProd").append("<tr><td>"+p.ref+"</td><td>"+p.nom+
+        $("#tBodyProd").append("<tr id='"+ p.ref +"'><td>"+p.ref+"</td><td>"+p.nom+
                                     "</td><td>"+p.prix+"</td></tr>");
+        $("#"+p.ref).on("click", function (evt){
+            let eltTdDuClick = evt.target; //api DOM
+            let parentTr = eltTdDuClick.parentNode;  //api DOM
+            console.log("click sur : " + parentTr.id);
+            let refProd="?"; 
+            let nomProd="?"; 
+            let prixProd =0;
+            $(parentTr).children("td").each (function (index) {
+                //ici (dans une fonction imbriquee dans .each) reférence le ième sous ellement un <td>
+                if(index == 0) refProd = $(this).html();
+                if(index==1) nomProd = $(this).html(); //api JQ
+                if(index==2) prixProd = $(this).html();
+            });
+            $("#message").html(nomProd + " " + prixProd);  //api JQ
+        })
         /*
         let ligne = "<tr>";
         for(let prop in p){ 
