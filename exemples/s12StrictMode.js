c2cd2b8ef80272a1b05b5ed526169505deb403b3
Renaud <renaudtoyer@gmail.com>
2024-05-30 17:13:10 +0200

    clearing to end up with td/tp correction only

--- exemples/s12StrictMode.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-var g1="abc";
-//g2 = "def";//ReferenceError: g2 is not defined (in strict mode)
-console.log("g1="+g1);
--- exemples/s3FonctionNbArgVariable.js
+++
@@ -1,9 +0,0 @@
-function fonctionANbArgVariable(...args){
-    let nbArgs = args.length;
-    console.log("nbArgs="+nbArgs);
-    console.log("premier_args="+  ( (nbArgs>=1)?args[0]:"non renseigne" ) )
-}
-
-fonctionANbArgVariable();
-fonctionANbArgVariable("novembre")
-fonctionANbArgVariable("novembre","decembre")
